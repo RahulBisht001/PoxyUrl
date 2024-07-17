498b9ff5d10aec79591a5c721ca04223eb0edc7c
RahulB <rahulbisht1012@gmail.com>
2024-07-17 15:11:05 +0530

    minor bug fix on dashboard UI

--- src/app/_components/LinkData.js
+++ src/app/_components/LinkData.js
@@ -112,7 +112,7 @@ const LinkData = () => {
                                                     </button>
                                                 </div>
 
-                                                <p className="my-2">{`${process.env.FRONTEND_URL}/${link.shortId}`}</p>
+                                                <p className="my-2">{`${process.env.FRONTEND_URL}/id/${link.shortId}`}</p>
                                                 <div className="flex justify-between items-center text-sm mt-4">
                                                     <Link
                                                         href={`${process.env.FRONTEND_URL}/${link.shortId}`}
